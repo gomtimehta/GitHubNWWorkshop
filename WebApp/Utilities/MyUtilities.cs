27446b337fb2c579a630ef5f52a23dd8e40efccb
Gomti Mehta <gomehta@microsoft.com>
2023-01-31 09:28:31 -0600

    GitHub copilot demo file

---
+++ WebApp/Utilities/MyUtilities.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml.Linq;
+
+namespace WebApp.Utilities
+{
+    public class MyUtilities
+    {
+
+        public static int CalculateDaysBetweenDates(DateTime startDate, DateTime endDate)
+        {
+            return (endDate - startDate).Days;
+        }
+
+        public static void FindAllImages()
+        {
+
+            var doc = XDocument.Load("index.xhml");
+
+            // find all images
+            var images = doc.Descendants("img");
+        }
+
+    }
+}
